48d6fa9ab939a34219de2f60837f5f3cecb6461f
Michael Fotiadis <michaelfotiadis@gmail.com>
2016-10-01 17:42:57 +0100

    Moving to a number picker

--- app/src/main/java/com/michaelfotiadis/deskalarm/dialogs/TimePickerDialogWrapper.java
+++ app/src/main/java/com/michaelfotiadis/deskalarm/dialogs/TimePickerDialogWrapper.java
@@ -1,9 +1,13 @@
 package com.michaelfotiadis.deskalarm.dialogs;
 
 import android.app.Activity;
-import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.widget.TimePicker;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.widget.NumberPicker;
+import android.widget.RelativeLayout;
 
 import com.michaelfotiadis.deskalarm.R;
 import com.michaelfotiadis.deskalarm.utils.log.AppLog;
@@ -13,9 +17,11 @@ import com.michaelfotiadis.deskalarm.utils.log.AppLog;
  *
  * @author Michael Fotiadis
  */
-public class TimePickerDialogWrapper implements TimePickerDialog.OnTimeSetListener {
+public class TimePickerDialogWrapper {
 
-    private final TimePickerDialog mDialog;
+    private static final int MAX_TIME = 120;
+
+    private final AlertDialog mDialog;
     private final int mOriginalSetting;
     private final SharedPreferences mSharedPreferences;
 
@@ -27,8 +33,17 @@ public class TimePickerDialogWrapper implements TimePickerDialog.OnTimeSetListen
 
         mSharedPreferences = sharedPreferences;
 
-        mOriginalSetting = mSharedPreferences.getInt(activity.getString(R.string.pref_alarm_interval_key),
-                activity.getResources().getInteger(R.integer.time_to_alarm));
+        final int preferencesTime = mSharedPreferences.getInt(
+                activity.getString(R.string.pref_alarm_interval_key),
+                activity.getResources().getInteger(R.integer.time_to_alarm)
+        );
+
+
+        if (preferencesTime > MAX_TIME) {
+            mOriginalSetting = MAX_TIME;
+        } else {
+            mOriginalSetting = preferencesTime;
+        }
 
         final int hour = mOriginalSetting / 60;
         int minute = mOriginalSetting % 60;
@@ -38,10 +53,55 @@ public class TimePickerDialogWrapper implements TimePickerDialog.OnTimeSetListen
             minute = 0;
         }
 
-        // get a 24 hour time picker
-        mDialog = new TimePickerDialog(activity, this, hour, minute, false);
-        mDialog.setTitle(activity.getString(R.string.dialog_set_interval_between_alarms));
+        final NumberPicker picker = new NumberPicker(activity);
+        picker.setMaxValue(MAX_TIME);
 
+
+
+
+        // get a 24 hour time picker
+        mDialog = initNumberPicker(activity, mOriginalSetting, MAX_TIME);
+
+    }
+
+    private AlertDialog initNumberPicker(final Context context, final int currentValue, final int maxValue) {
+        final RelativeLayout linearLayout = new RelativeLayout(context);
+        final NumberPicker numberPicker = new NumberPicker(context);
+        numberPicker.setMaxValue(maxValue);
+        numberPicker.setMinValue(1);
+        numberPicker.setValue(currentValue);
+        numberPicker.setBackgroundColor(ContextCompat.getColor(context, R.color.md_grey_100));
+
+        final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(50, 50);
+        final RelativeLayout.LayoutParams numberPickerParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(4, 4, 4, 4);
+        numberPickerParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+
+        linearLayout.setLayoutParams(params);
+        linearLayout.addView(numberPicker, numberPickerParams);
+
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+        alertDialogBuilder.setTitle(context.getString(R.string.dialog_set_interval_between_alarms));
+        alertDialogBuilder.setView(linearLayout);
+        alertDialogBuilder
+                .setCancelable(true)
+                .setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(final DialogInterface dialog, final int id) {
+                                onTimeSet(numberPicker.getValue());
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(final DialogInterface dialog,
+                                                final int id) {
+                                dialog.cancel();
+                            }
+                        });
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+
+        return alertDialog;
     }
 
     public void show() {
@@ -52,11 +112,10 @@ public class TimePickerDialogWrapper implements TimePickerDialog.OnTimeSetListen
         mDialog.dismiss();
     }
 
-    @Override
-    public void onTimeSet(final TimePicker view, final int hourOfDay, final int minute) {
-        AppLog.i("Value selected " + hourOfDay + " : " + minute);
+    private void onTimeSet(final int minutes) {
+        int timeToAlarm = minutes;
 
-        int timeToAlarm = hourOfDay * 60 + minute;
+        AppLog.d("Time to alarm= " + timeToAlarm);
 
         if (timeToAlarm == mOriginalSetting) {
             mDialog.dismiss();
