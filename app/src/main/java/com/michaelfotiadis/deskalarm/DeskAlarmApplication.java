9269aaa5ac974b397d1485e19a5c43a0bca7499e
Michael Fotiadis <michaelfotiadis@gmail.com>
2017-01-19 15:44:02 +0000

    Added Crashlytics

--- app/src/main/java/com/michaelfotiadis/deskalarm/DeskAlarmApplication.java
+++ app/src/main/java/com/michaelfotiadis/deskalarm/DeskAlarmApplication.java
@@ -2,13 +2,17 @@ package com.michaelfotiadis.deskalarm;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.michaelfotiadis.deskalarm.utils.log.AppLog;
+
+import io.fabric.sdk.android.Fabric;
 
 public class DeskAlarmApplication extends Application {
     @Override
     public void onCreate() {
         AppLog.d("Starting Application");
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
     }
 
 }
